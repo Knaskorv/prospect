d310a78e5c4f64f3bfbd66975414c44d1bd4e604
unknown <joakim.olsson@tritech.se>
2018-11-03 19:10:36 +0100

    setup frontend. Including vue, vuetify and vue router

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>TRITECH</span>
+        <span class="font-weight-light">PROSPECT v.2</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <v-layout>
+        Tritech prospect v.2
+      </v-layout>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
